0ec63426b89774706dff0b4bd95676af9a1d1aa0
THE PUNISHER <artyom.pennini@gmail.com>
2026-08-02 13:46:38 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 import chalk from 'chalk'
 
 global.owner = [
-  ['393784409415', 'Elixir-39', true],
-  ['639752917233', 'Elixir-63', true],
-  ['13018239629', 'ElixirBot', true],
-  ['79524931364', 'Punisher', true],
-  ['212785655331', 'Ghost', true]
+  ['xxxx', 'xxxx', true],
+  ['xxxx', 'xxxx', true],
+  ['xxxx', 'xxxx', true],
+  ['573117824583', 'Punisher', true],
+  ['22652522149', 'Nemy', true]
 ]
 
-global.nomebot = '𝐎𝐏𝐏𝐑𝐈𝐌𝐈 𝐁𝐎𝐓'
-global.wm = '𝐎𝐏𝐏𝐑𝐈𝐌𝐈 𝐁𝐎𝐓'
-global.autore = 'opprimibot'
+global.nomebot = 'PUNISCI-BOT'
+global.wm = 'PUNISCI-BOT'
+global.autore = 'puniscibot'
