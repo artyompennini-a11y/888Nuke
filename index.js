df5521b2d53b24564af095f31b6f0a1bb0e87db9
THE PUNISHER <artyom.pennini@gmail.com>
2026-08-02 13:49:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ async function epicStartup() {
   
   await sleep(300);
   
-  await typeWriterBig('OPPRIMI\nBOT\n2026', 120);
+  await typeWriterBig('PUNISCI\nBOT\n2026', 120);
   
   await sleep(400);
   
@@ -136,7 +136,7 @@ async function epicStartup() {
   await typeWriter('                     Ultimo Aggiornamento: 21/05/2026', 40, '\x1b[33m');
   
   console.log('\n');
-  await typeWriter('                    opprimibot', 35, '\x1b[36m');
+  await typeWriter('                    puniscibot', 35, '\x1b[36m');
   
   console.log('\n\n');
   console.log('\x1b[90m' + '━'.repeat(70) + '\x1b[0m');
